f27cb6d0d81f5b1dc0c17d02475b8fef58d857c4
soultka <soultka>
2018-02-13 20:01:04 +0900

    rdkit with Maccs

--- beta_ractam_sklearn_rdkit.py
+++ beta_ractam_sklearn_rdkit.py
@@ -1,10 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 26 21:49:49 2018
-
-@author: KOREA
-"""
-import pandas as pd
+﻿import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix 
@@ -12,17 +6,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from rdkit.Chem.EState import AtomTypes,EState,EState_VSA,Fingerprinter
 from rdkit import Chem
+from rdkit.Chem import MACCSkeys
 
 df = pd.read_csv('bioactivity-18_5_57_13.txt', delimiter='\t')
 columns = ['MOLWEIGHT', 'ALOGP', 'PSA',
            'NUM_RO5_VIOLATIONS', 
-            'PUBLISHED_VALUE' 
+           
            ]
 df = df.drop_duplicates(['CMPD_CHEMBLID']) 
 # 중복 제거 666개 
 df = df.dropna(subset=['CANONICAL_SMILES','MOLWEIGHT', 'ALOGP', 'PSA',
            'NUM_RO5_VIOLATIONS', 
-            'PUBLISHED_VALUE' ])
+           ])
 # NAN 제거 655
 y = np.array(df[['ACTIVITY_COMMENT']])
 X_raw = np.array(df[columns])
@@ -42,16 +37,25 @@ for sm in Smiles.astype(str):
 # Mol rdkit의 mol 객체 
 
 Fingerprint=[]
+MKbits = []
 for j,mol in enumerate(Mol):
-        Fingerprint.append(Fingerprinter.FingerprintMol(mol))
-     
+    Fingerprint.append(Fingerprinter.FingerprintMol(mol))
+    bv = MACCSkeys.GenMACCSKeys(mol)
+    keysbits=tuple(bv.GetOnBits())
+    MK = np.zeros(166).astype(int)
+    for bit in keysbits:
+        MK[bit-1] = 1
+    MKbits.append(MK)    
+
+MKbits = np.array(MKbits)
+Maccs = MKbits.reshape(-1,166)    
 Fingerprint = np.array(Fingerprint).reshape(-1,158)
 
 
-XwithFingerprint=np.column_stack((X_raw,Fingerprint))
+X_Finger_Maccs=np.column_stack((X_raw,Fingerprint,Maccs))
 
-X_train , X_test , y_train , y_test = train_test_split(XwithFingerprint,y)
-clf = MLPClassifier(hidden_layer_sizes=(100,100,100,100),max_iter=10000)  
+X_train , X_test , y_train , y_test = train_test_split(X_Finger_Maccs,y)
+clf = MLPClassifier(hidden_layer_sizes=(10,10,10,10,10,10,10,10,10,10,10,10), max_iter=10000)  
 clf.fit(X_train,y_train)
 y_pred = clf.predict(X_test)
 
